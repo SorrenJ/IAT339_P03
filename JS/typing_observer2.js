d7150533fa4432a378656b3e3e2ddf9f8405f8a2
SorrenJ <sjao@sfu.ca>
2023-04-10 17:39:47 -0700

    removed offsetwidth by disabling typing anime and placing blossom wrapper for project card

--- JS/typing_observer2.js
+++ JS/typing_observer2.js
@@ -1,23 +1,4 @@
-// const animateText = document.querySelector('.typing-demo');
-// const section = document.querySelector('.wrapper');
-// const options ={
-//     root: null,
-//     threshold: 0.5,
-//     // rootMargin: "-150px"
-    
-//     };
-    
-// const observer = new IntersectionObserver(entries => {
-//   if (entries[0].isIntersecting) {
-//     animateText.style.opacity = 1;
-//     animateText.style.animation = 'typing 2s steps(22), blink .5s step-end infinite alternate;';
-//   } else {
-//     animateText.style.opacity = 0;
-//     animateText.style.animation = 'none';
-//   }
-// });
 
-// observer.observe(section);
 
 const typing1 = document.querySelectorAll('.typing-demo1');
 
